b3a4c4045716d93d6a5afda79605f12e610a70d7
Silas Snider <swsnider@gmail.com>
2013-08-20 21:13:11 -0700

    Work so far
    Signed-off-by: Silas Snider <swsnider@gmail.com>

--- api.go
+++ api.go
@@ -1,25 +1,37 @@
 package golink
 
-import "time"
+import (
+	"fmt"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"sort"
+	"time"
+)
 
 type API struct {
-	BaseURL, KeyID, VCode string
-	Cache                 APICache
+	BaseURL, KeyID, VCode            string
+	Cache                            APICache
+	lastCachedUntil, lastCurrentTime time.Time
+	Client URLFetcher
 }
 
+type URLFetcher func(path string, params url.Values) (result *http.Response, err error)
+
 type APICache interface {
-	Get(k string) interface{}
-	Put(k string, v interface{}, duration time.Duration)
+	Get(k string) []byte
+	Put(k string, v []byte, duration time.Duration)
 }
 
 type InMemoryCacheValue struct {
-	V          interface{}
+	V          []byte
 	Expiration time.Time
 }
 
 type InMemoryAPICache map[string]*InMemoryCacheValue
 
-func (c InMemoryAPICache) Get(k string) interface{} {
+func (c InMemoryAPICache) Get(k string) []byte {
 	r := c[k]
 	if r == nil {
 		return nil
@@ -31,9 +43,83 @@ func (c InMemoryAPICache) Get(k string) interface{} {
 	return r.V
 }
 
-func (c InMemoryAPICache) Set(k string, v interface{}, duration time.Duration) {
+func (c InMemoryAPICache) Put(k string, v []byte, duration time.Duration) {
 	c[k] = &InMemoryCacheValue{V: v, Expiration: time.Now().Add(duration)}
 }
 
+type EveError struct {
+	Msg  string `xml:"chardata"`
+	Code string `xml:"code,attr"`
+}
+
+type GenericAPIResponse struct {
+	Result      []byte   `xml:"result,innerxml"`
+	CurrentTime string   `xml:"currentTime"`
+	CachedUntil string   `xml:"cachedUntil"`
+	Error       EveError `xml:"error"`
+}
+
+func NewAPI() *API {
+	return &API{}
+}
+
 //Request a specific path from the EVE API.
-func (a *API) Get(path string, params map[string]string) error
+func (a *API) Get(path string, params url.Values) ([]byte, error) {
+	if a.KeyID != "" {
+		params["keyID"] = []string{a.KeyID}
+		params["vCode"] = []string{a.VCode}
+	}
+	cacheKey := genCacheKey(path, params)
+	response := a.Cache.Get(cacheKey)
+	cached := response != nil
+	if !cached {
+		r, err := a.Client(fmt.Sprintf("https://%v/%v.xml.aspx", a.BaseURL, path), params)
+		if err != nil {
+			return nil, err
+		}
+		defer r.Body.Close()
+		response, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	xmlResponse := GenericAPIResponse{}
+	err := xml.Unmarshal(response, &xmlResponse)
+	if err != nil {
+		return nil, err
+	}
+	currentTime, err := parseEveTs(xmlResponse.CurrentTime)
+	if err != nil {
+		return nil, err
+	}
+	a.lastCurrentTime = currentTime
+	expiresTime, err := parseEveTs(xmlResponse.CachedUntil)
+	if err != nil {
+		return nil, err
+	}
+	a.lastCachedUntil = expiresTime
+
+	if !cached {
+		a.Cache.Put(cacheKey, response, expiresTime.Sub(currentTime))
+	}
+
+	if xmlResponse.Error.Code != "" {
+		return nil, fmt.Errorf("API reported error with code %v and message \"%v\"", xmlResponse.Error.Code, xmlResponse.Error.Msg)
+	}
+
+	return xmlResponse.Result, nil
+}
+
+func genCacheKey(path string, params url.Values) string {
+	ks := make([]string, 0)
+	for k, v := range params {
+		ks = append(ks, fmt.Sprint(k, v))
+	}
+	sort.StringSlice(ks).Sort()
+	return fmt.Sprintf("%v#%v", path, ks)
+}
+
+func parseEveTs(in string) (time.Time, error) {
+	return time.Parse("2006-01-02 15:04:05", in)
+}
---
+++ api_test.go
@@ -0,0 +1,65 @@
+package golink
+
+import (
+"net/url"
+"testing"
+)
+
+func TestTimeParse(t *testing.T) {
+  ts, err := parseEveTs("2012-06-12 12:04:33")
+  if err != nil {
+    t.Error(err)
+  }
+  if ts.Unix() != 1339502673 {
+    t.Errorf("Parsed time incorrect. Got: %+v", ts)
+  }
+}
+
+func TestCache(t *testing.T) {
+  cache := make(InMemoryAPICache)
+  cache.Put("foo", []byte("bar"), 3600)
+  v := cache.Get("foo")
+  if string(v) != "bar" {
+    t.Errorf("Incorrect cache value for foo: %v", v)
+  }
+}
+
+func TestExpire(t *testing.T) {
+  cache := make(InMemoryAPICache)
+  cache.Put("foo", []byte("bar"), -1)
+  v := cache.Get("foo")
+  if string(v) != "" {
+    t.Errorf("Incorrect cache value for foo: %v", v)
+  }
+}
+
+func TestCacheKey(t *testing.T) {
+  if genCacheKey("foo/bar", url.Values{"a": []string{"1"}, "b": []string{"2"}}) != genCacheKey("foo/bar", url.Values{"b":[]string{"2"}, "a":[]string{"1"}}){
+    t.Error("genCacheKey does not sort map keys.")
+  }
+}
+
+const testXML = `
+<?xml version='1.0' encoding='UTF-8'?>
+<eveapi version="2">
+    <currentTime>2009-10-18 17:05:31</currentTime>
+    <result>
+        <rowset>
+            <row foo="bar" />
+            <row foo="baz" />
+        </rowset>
+    </result>
+    <cachedUntil>2009-11-18 17:05:31</cachedUntil>
+</eveapi>
+`
+
+const errXML = `
+<?xml version='1.0' encoding='UTF-8'?>
+<eveapi version="2">
+    <currentTime>2009-10-18 17:05:31</currentTime>
+    <error code="123">
+        Test error message.
+    </error>
+    <cachedUntil>2009-11-18 19:05:31</cachedUntil>
+</eveapi>
+`
